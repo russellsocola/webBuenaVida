2ba7cc74fc19abc366217854a017db553b412aaf
russellsocola <russell999nahum@gmail.com>
2022-03-26 21:04:05 -0500

    profesionalizando el codigo

--- js/form.js
+++ js/form.js
@@ -1,39 +1,47 @@
 var button = document.querySelector("#adicionar-paciente");
+
 button.addEventListener("click", function(event){
     event.preventDefault();
+
     var form = document.querySelector("#form-adicionar");
-
-    var nombre = form.nombre.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var talba = document.querySelector("#tabla-pacientes");    
-    
-    console.log(nombre);
-    console.log(peso);
-    console.log(altura);
-    console.log(gordura);
-
-    pacienteTr = document.createElement("tr");
-    nombreTd = document.createElement("td");
-    pesoTd = document.createElement("td");
-    alturaTd = document.createElement("td");
-    gorduraTd = document.createElement("td");
-    imcTd = document.createElement("td");
-
-    nombreTd.textContent= nombre;
-    pesoTd.textContent= peso;
-    alturaTd.textContent= altura;
-    gorduraTd.textContent= gordura;
-    imcTd.textContent=calcularIMC(peso,altura);
-
-    pacienteTr.appendChild(nombreTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    var paciente = capturarDatosPaciente(form);
+    var pacienteTr = construirTr(paciente);
+    var talba = document.querySelector("#tabla-pacientes");
 
     talba.appendChild(pacienteTr);
-    
+    form.reset();
 });
+
+function capturarDatosPaciente(form){
+    //capturando datos del formulario
+    var paciente = {
+        nombre :  form.nombre.value,
+        peso : form.peso.value,
+        altura : form.altura.value,
+        gordura : form.gordura.value,
+        imc : calcularIMC(form.peso.value,form.altura.value)
+    }
+    return paciente;
+}
+
+function construirTr(paciente){
+
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+    
+    pacienteTr.appendChild(construirTd(paciente.nombre, "info-nombre"));
+    pacienteTr.appendChild(construirTd(paciente.peso,"info-peso"));
+    pacienteTr.appendChild(construirTd(paciente.altura,"info-altura"));
+    pacienteTr.appendChild(construirTd(paciente.gordura,"info-gordura"));
+    pacienteTr.appendChild(construirTd(paciente.imc,"info-imc"));
+
+    return pacienteTr;
+}
+
+function construirTd(dato,clase){
+    var td = document.createElement("td");
+    td.classList.add(clase);
+    td.textContent = dato;
+
+    return td;
+}
