be7580c73b527602df13e7b922d4c037f853e1b2
russellsocola <russell999nahum@gmail.com>
2022-04-02 03:12:55 -0500

    Validando errores en el Ajax.

--- js/buscarPacientes.js
+++ js/buscarPacientes.js
@@ -4,14 +4,22 @@ buscar.addEventListener("click", function(){
    var xhr = new XMLHttpRequest();
    xhr.open("GET","https://alura-es-cursos.github.io/api-pacientes/pacientes.json");
    xhr.addEventListener("load", function(){
-       var respuesta = xhr.responseText;       
-       var pacientes = JSON.parse(respuesta);
+       var errorAjax = document.querySelector("#error-ajax");
+       if(xhr.status == 200){
+            errorAjax.classList.add("invisible");
+            var respuesta = xhr.responseText;       
+            var pacientes = JSON.parse(respuesta);
+    
+            pacientes.forEach(function(paciente){
+            adicionarPacienteEnLaTabla(paciente);
+            });
 
-       pacientes.forEach(function(paciente){
-        adicionarPacienteEnLaTabla(paciente);
-        console.log(paciente);
-       });
-   })
+        }else{
+           errorAjax.classList.remove("invisible");
+           console.log(xhr.status);
+           console.log(xhr.responseText);
+       }
+   });
 
    xhr.send();
 });
